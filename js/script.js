73bfa24896ea1a8647be061b32f30b0d2fc18a95
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-25 21:21:57 +0530

    Days added

--- js/script.js
+++ js/script.js
@@ -19,4 +19,5 @@ function showTime() {
 
     let months = ["January", "February", "March", "April", "May", "June", 
                     "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 }

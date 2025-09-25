b1b58ab05aac15a2aab48e0e53e482661ace679b
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-25 21:23:56 +0530

    Dayname monthName taken from inbuilt fun

--- js/script.js
+++ js/script.js
@@ -20,4 +20,8 @@ function showTime() {
     let months = ["January", "February", "March", "April", "May", "June", 
                     "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let dayName = days[today.getDay()];
+    let monthName = months[today.getMonth()];
+    let date = dayName + ", " + monthName + " " + today.getDate() + ", " + today.getFullYear();
 }
